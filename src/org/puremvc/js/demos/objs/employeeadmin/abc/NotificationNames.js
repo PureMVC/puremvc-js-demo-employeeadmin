790124bd0f8c8a9399994a878ef37ead34732ea6
Frederic Saunier <frederic.saunier@puremvc.org>
2012-01-09 22:06:30 +0000

    Updated to Employee Admin Demo with Objs v2.1.1 using jQuery, jQuery UI, YUICompressor and Ant

---
+++ src/org/puremvc/js/demos/objs/employeeadmin/abc/NotificationNames.js
@@ -0,0 +1,23 @@
+/*
+ PureMVC Javascript Objs Employee Admin Demo for jQuery
+ by Frederic Saunier <frederic.saunier@puremvc.org> 
+ PureMVC - Copyright(c) 2006-11 Futurescale, Inc., Some rights reserved. 
+ Your reuse is governed by the Creative Commons Attribution 3.0 License
+*/
+
+/**
+ * @class
+ * Defines <code>Notification</code> names for the application.
+ */
+var NotificationNames = Objs("org.puremvc.js.demos.objs.employeeadmin.abc.NotificationNames",{});
+
+NotificationNames.STARTUP = "startup";
+NotificationNames.NEW_USER = "newUser";
+NotificationNames.DELETE_USER = "deleteUser";
+NotificationNames.CANCEL_SELECTED = "cancelSelected";
+NotificationNames.USER_SELECTED = "userSelected";
+NotificationNames.USER_ADDED = "userAdded";
+NotificationNames.USER_UPDATED = "userUpdated";
+NotificationNames.USER_DELETED = "userDeleted";
+NotificationNames.ADD_ROLE = "addRole";
+NotificationNames.ADD_ROLE_RESULT = "addRoleResult";
---
+++ src/org/puremvc/js/demos/objs/employeeadmin/abc/ProxyNames.js
@@ -0,0 +1,15 @@
+/*
+ PureMVC Javascript Objs Employee Admin Demo for jQuery
+ by Frederic Saunier <frederic.saunier@puremvc.org> 
+ PureMVC - Copyright(c) 2006-11 Futurescale, Inc., Some rights reserved. 
+ Your reuse is governed by the Creative Commons Attribution 3.0 License
+*/
+
+/**
+ * @class
+ * Defines <code>Proxy</code> names for the application.
+ */
+var ProxyNames = Objs("org.puremvc.js.demos.objs.employeeadmin.abc.ProxyNames",{});
+
+ProxyNames.ROLE_PROXY = "roleProxy";
+ProxyNames.USER_PROXY = "userProxy";
