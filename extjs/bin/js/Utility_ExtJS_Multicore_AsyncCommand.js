296dccc1425c1cd50dbc603f1f6168cf952ca3fc
Tony DeFusco <tony.defusco@puremvc.org>
2012-01-19 02:45:30 +0000

    Updated to use native version of multi-core AsyncCommand Utility with updated ExtJS-3 adapter.
    bin/index.html
    bin/js/PureMVC_JS_ExtJS_3_adapter.js
    bin/js/Utility_ExtJS_Multicore_AsyncCommand.js

--- extjs/bin/js/Utility_ExtJS_Multicore_AsyncCommand.js
+++ extjs/bin/js/Utility_ExtJS_Multicore_AsyncCommand.js
@@ -1,243 +1,271 @@
+/**
+ * @fileOverview
+ * PureMVC multicore native JavaScript port.
+ * @author David Foley | david.foley@puremvc.org
+ */
+(function (scope){
+	
+	// create the org.puremvc.js object hierarchy
+	// if the hierarchy does not yet exist
+	if (null == scope['org'])
+	{
+		scope.org= {};
+	}
+	
+	if (null == scope['org']['puremvc'])
+	{
+		scope['org']['puremvc']= {};
+	}
+	
+	if (null == scope['org']['puremvc']['js'])
+	{
+		scope['org']['puremvc']['js']= {};
+	}
+	
+	if (null == scope['org']['puremvc']['js']['multicore'])
+	{
+		scope['org']['puremvc']['js']['multicore'] = {};
+	}
+
+ 	/* implementation begin */
+	
+	
+
 /*
- PureMVC Javascript AsyncCommand Utility for ExtJS port
+ PureMVC Javascript AsyncCommand Utility for native port
  by Tony DeFusco <tony.defusco@puremvc.org>
- PureMVC - Copyright(c) 2006-11 Futurescale, Inc., Some rights reserved.
+ PureMVC - Copyright(c) 2006-12 Futurescale, Inc., Some rights reserved.
  Your reuse is governed by the Creative Commons Attribution 3.0 License
  */
 
 /**
- * @lends Puremvc.patterns.AsyncCommand.prototype
+ * @class org.puremvc.js.multicore.patterns.command.AsyncCommand
+ * @extends org.puremvc.js.multicore.patterns.command.SimpleCommand
+ *
+ * A base #AsyncCommand implementation.
+ *
+ * Your subclass should override the #execute method where your business logic
+ * will handle the
+ * {@link org.puremvc.js.multicore.patterns.observer.Notification Notification}
+ * 
+ * @constructor
  */
-Ext.namespace('Puremvc.patterns');
-Puremvc.patterns.AsyncCommand = Ext.extend(Puremvc.patterns.SimpleCommand, {
+function AsyncCommand() {
+  this.isAsyncCommand = true;
+}
 
-  /**
-   * The <code>AsyncMacroCommand</code> method to call on completion.
-   */
-  onComplete: null,
+AsyncCommand.prototype = new org.puremvc.js.multicore.patterns.observer.Notifier();
+AsyncCommand.prototype.constructor = AsyncCommand;
 
-  /**
-   * @extends Puremvc.patterns.SimpleCommand
-   * @class <P>A base <code>AsyncCommand</code> implementation.</P>
-   *
-   * <P>
-   * Your subclass should override the <code>execute</code>
-   * method where your business logic will handle the notification. </P>
-   *
-   * @see Puremvc.patterns.AsyncMacroCommand
-   *
-   * @author Tony DeFusco
-   *
-   * @constructs
-   */
-  constructor: function() {
-    Puremvc.patterns.AsyncCommand.superclass.constructor.call(this);
-    this.isAsyncCommand = true;
-  },
+/**
+ * The #AsyncMacroCommand method to call on completion.
+ */
+AsyncCommand.prototype.onComplete = null;
 
-  /**
-   * Registers the callback for a parent <code>AsyncMacroCommand</code>.
-   *
-   * @param {Function} value The <code>AsyncMacroCommand</code> method to call on completion.
-   */
-  setOnComplete: function(value/*Function*/) {
-    this.onComplete = value;
-  },
+/**
+ * Registers the callback for a parent #AsyncMacroCommand.
+ *
+ * @param {Function} value The #AsyncMacroCommand method to call on completion.
+ */
+AsyncCommand.prototype.setOnComplete = function(value/*Function*/) {
+  this.onComplete = value;
+};
 
-  /**
-   * Notify the parent <code>AsyncMacroCommand</code> that this command is complete.
-   * <P>
-   * Call this method from your subclass to signify that your asynchronous command
-   * has finished.
-   */
-  commandComplete: function() {
-    if (Ext.isFunction(this.onComplete)) {
+/**
+ * Notify the parent #AsyncMacroCommand that this command is complete.
+ *
+ * Call this method from your subclass to signify that your asynchronous command
+ * has finished.
+ */
+AsyncCommand.prototype.commandComplete = function() {
+  if (this.onComplete && Object.prototype.toString.call(this.onComplete) === '[object Function]') {
+    this.onComplete();
+  }
+};
+
+/*
+ PureMVC Javascript AsyncCommand Utility native port
+ by Tony DeFusco <tony.defusco@puremvc.org>
+ PureMVC - Copyright(c) 2006-12 Futurescale, Inc., Some rights reserved.
+ Your reuse is governed by the Creative Commons Attribution 3.0 License
+ */
+
+/**
+ * @class org.puremvc.js.multicore.patterns.command.AsyncMacroCommand
+ * @extends org.puremvc.js.multicore.patterns.observer.Notifier
+ * 
+ * A base command implementation that executes other commands asynchronously,
+ * such as {@link org.puremvc.js.multicore.patterns.command.SimpleCommand 
+ * SimpleCommand} or 
+ * {@link org.puremvc.js.multicore.patterns.command.MacroCommand MacroCommand}
+ * subclasses.
+ *  
+ * An AsyncMacroCommand maintains a list of command constructor references 
+ * called *SubCommands*.
+ * 
+ * When #execute is called, the AsyncMacroCommand
+ * caches a reference to the 
+ * {@link org.puremvc.js.multicore.patterns.observer.Notification Notification} 
+ * and calls #nextCommand on each of its *SubCommands* in turn.
+ *
+ * If there are still *SubCommands* to be executed, the #nextCommand method 
+ * instantiates and calls #execute on each of its *SubCommands* in turn. Each 
+ * *SubCommand* will be passed a reference to the original 
+ * {@link org.puremvc.js.multicore.patterns.observer.Notification Notification}
+ * that was passed to the #AsyncMacroCommand's #execute method. If the
+ * *SubCommand* to execute is an #AsyncCommand, the next *SubCommand* will not 
+ * be executed until the previous *AsyncCommand has called its 
+ * *commandComplete* method.
+ * 
+ * Unlike 
+ * {@link org.puremvc.js.multicore.patterns.command.AsyncCommand AsyncCommand}
+ * and 
+ * {@link org.puremvc.js.multicore.patterns.command.SimpleCommand SimpleCommand},
+ * your subclass should not override #execute, but instead, should override the 
+ * #initializeAsyncMacroCommand method, calling #addSubCommand once for each 
+ * *SubCommand* to be executed.
+ *
+ * You should not need to define a constructor, instead, override the 
+ * #initializeAsyncMacroCommand method.
+ *
+ * If your subclass does define a constructor, be sure to call "super" like so
+ * 
+ *     function MyMacroCommand ()
+ *     {
+ *         AsyncMacroCommand.call(this);
+ *     };
+ * @constructor
+ */
+function AsyncMacroCommand() {
+  this.isAsyncCommand = true;
+  this.subCommands = new Array();
+  this.initializeAsyncMacroCommand();
+}
+
+/* subclass Notifier */
+AsyncMacroCommand.prototype = new org.puremvc.js.multicore.patterns.observer.Notifier();
+AsyncMacroCommand.prototype.constructor = AsyncMacroCommand;
+
+/**
+ * An array of <code>AsyncCommand</code>s or subclasses of.
+ * @private
+ * @type {Array.<SimpleCommand|MacroCommand|AsyncCommand|AsyncMacroCommand>}
+ */
+AsyncMacroCommand.prototype.subCommands = null;
+
+/**
+ * The <code>Notification</code> object to be passed to each <i>SubCommand</i>.
+ * @type {Object}
+ * @private
+ */
+AsyncMacroCommand.prototype.notification = null;
+
+/**
+ * The <code>AsyncMacroCommand</code> method to call on completion.
+ * @type {Function}
+ * @private
+ */
+AsyncMacroCommand.prototype.onComplete = null;
+
+/**
+ * Initialize the #AsyncMacroCommand
+ *
+ * In your subclass, override this method to
+ * initialize the #AsyncMacroCommand<'s *SubCommand*
+ * list with #ICommand class references.
+ *
+ * <listing>
+ *    // Initialize MyMacroCommand
+ *    function initializeAsyncMacroCommand()
+ *    {
+ *      addSubCommand(myapp.controller.FirstCommand);
+ *      addSubCommand(myapp.controller.SecondCommand);
+ *      addSubCommand(myapp.controller.ThirdCommand);
+ *    }
+ * </listing>
+ *
+ * Note that *SubCommands* may be any #Command implementor,
+ * #AsyncMacroCommands, #AsyncCommands, #MacroCommands or #SimpleCommands are 
+ * all acceptable.
+ */
+AsyncMacroCommand.prototype.initializeAsyncMacroCommand = function() {};
+
+/**
+ * Add a *SubCommand*.
+ *
+ * The *SubCommands* will be called in First In/First Out (FIFO) order.
+ *
+ * @param {Class} commandClassRef a reference to the #Class of the #ICommand.
+ */
+AsyncMacroCommand.prototype.addSubCommand = function(commandClassRef/*Class*/) {
+  this.subCommands.push(commandClassRef);
+};
+
+/**
+ * Registers the callback for a parent #AsyncMacroCommand.
+ *
+ * @param {Function} value The #AsyncMacroCommand method to call on completion.
+ */
+AsyncMacroCommand.prototype.setOnComplete = function(value /*Function*/) {
+  this.onComplete = value.createDelegate(this);
+};
+
+/**
+ * Starts execution of this #AsyncMacroCommand's *SubCommands*.
+ *
+ * The *SubCommands* will be called in First In/First Out (FIFO) order.
+ *
+ * @param {org.puremvc.js.multicore.patterns.observer.Notification}
+  * notification the #Notification object to be passed to each *SubCommand*.
+ */
+AsyncMacroCommand.prototype.execute = function(notification /*Notification*/) {
+  this.notification = notification;
+  this.nextCommand();
+};
+
+/**
+ * Execute this #AsyncMacroCommand's next *SubCommand*.
+ *
+ * If the next *SubCommand* is asynchronous, a callback is registered for
+ * the command completion, else the next command is run.
+ */
+AsyncMacroCommand.prototype.nextCommand = function() {
+  if (this.subCommands.length > 0) {
+    var commandClassRef/*Class*/ = this.subCommands.shift();
+    var commandInstance/*Object*/ = new commandClassRef();
+    var isAsync/*Boolean*/ = commandInstance.isAsyncCommand;
+
+    if (isAsync) {
+      commandInstance.setOnComplete(this.nextCommand.createDelegate(this));
+    }
+    commandInstance.initializeNotifier(this.multitonKey);
+    commandInstance.execute(this.notification);
+
+    if (!isAsync) {
+      this.nextCommand();
+    }
+  }
+  else {
+    if (this.onComplete != null) {
       this.onComplete();
     }
+
+    this.notification = null;
+    this.onComplete = null;
   }
-});
+};
 
-/*
- PureMVC Javascript AsyncCommand Utility for ExtJS port
- by Tony DeFusco <tony.defusco@puremvc.org>
- PureMVC - Copyright(c) 2006-11 Futurescale, Inc., Some rights reserved.
- Your reuse is governed by the Creative Commons Attribution 3.0 License
- */
-
-/**
- * @lends Puremvc.patterns.AsyncMacroCommand.prototype
- */
-Ext.namespace('Puremvc.patterns');
-Puremvc.patterns.AsyncMacroCommand = Ext.extend(Puremvc.patterns.Notifier, {
-
-  /**
-   * An array of <code>AsyncCommand</code>s or subclasses of.
-   * @type Array
-   * @private
-   */
-  subCommands: null,
-
-  /**
-   * The <code>Notification</code> object to be passed to each <i>SubCommand</i>.
-   * @type Object
-   * @private
-   */
-  notification: null,
-
-  /**
-   * The <code>AsyncMacroCommand</code> method to call on completion.
-   * @type Function
-   * @private
-   */
-  onComplete: null,
-
-  /**
-   * @extends Puremvc.patterns.Notifier
-   * @class <P>A base command implementation that executes other
-   * commands asynchronously.</P>
-   *
-   * <P>
-   * An <code>AsyncMacroCommand</code> maintains a list of
-   * command Class references called <i>SubCommands</i>.</P>
-   *
-   * <P>
-   * When <code>execute</code> is called, the <code>AsyncMacroCommand</code>
-   * caches a reference to the <notification and calls
-   * <code>nextCommand</code>.</P>
-   *
-   * <P>
-   * If there are still <i>SubCommands</i>'s to be executed,
-   * the <code>nextCommand</code> method instantiates and calls <code>execute</code>
-   * on each of its <i>SubCommands</i> in turn. Each <i>SubCommand</i> will be passed
-   * a reference to the original notification that was passed to the
-   * <code>AsyncMacroCommand</code>'s <code>execute</code> method. If the
-   * <i>SubCommand</i> to execute is an <code>AsyncCommand</code>, the
-   * next <i>SubCommand</i> will not be executed until the previous
-   * <code>AsyncCommand</code> has called its <i>commandComplete</i> method.</P>
-   *
-   * <P>
-   * Unlike <code>AsyncCommand</code> and <code>SimpleCommand</code>, your subclass
-   * should not override <code>execute</code>, but instead, should
-   * override the <code>initializeAsyncMacroCommand</code> method,
-   * calling <code>addSubCommand</code> once for each <i>SubCommand</i>
-   * to be executed.</P>
-   *
-   * <P>
-   * You should not need to define a constructor,
-   * instead, override the <code>initializeAsyncMacroCommand</code>
-   * method.</P>
-   *
-   * <P>
-   * If your subclass does define a constructor, be
-   * sure to call the superclass constructor anyway.</P>
-   *
-   * @see Puremvc.patterns.AsyncCommand
-   *
-   * @author Tony DeFusco
-   *
-   * @constructs
-   */
-  constructor: function() {
-    Puremvc.patterns.AsyncMacroCommand.superclass.constructor.call(this);
-    this.isAsyncCommand = true;
-    this.subCommands = new Array();
-    this.initializeAsyncMacroCommand();
-  },
-
-  /**
-   * Initialize the <code>AsyncMacroCommand</code>.
-   *
-   * <P>
-   * In your subclass, override this method to
-   * initialize the <code>AsyncMacroCommand</code>'s <i>SubCommand</i>
-   * list with <code>ICommand</code> class references.
-   * </P>
-   *
-   * <listing>
-   *    // Initialize MyMacroCommand
-   *    function initializeAsyncMacroCommand()
-   *    {
-   *      addSubCommand(myapp.controller.FirstCommand);
-   *      addSubCommand(myapp.controller.SecondCommand);
-   *      addSubCommand(myapp.controller.ThirdCommand);
-   *    }
-   * </listing>
-   *
-   * <P>
-   * Note that <i>SubCommand</i>s may be any <code>Command</code> implementor,
-   * <code>AsyncMacroCommand</code>s, <code>AsyncCommand</code>s,
-   * <code>MacroCommand</code>s or <code>SimpleCommands</code> are all acceptable.
-   */
-  initializeAsyncMacroCommand: function() {
-
-  },
-
-  /**
-   * Add a <i>SubCommand</i>.
-   *
-   * <P>
-   * The <i>SubCommands</i> will be called in First In/First Out (FIFO)
-   * order.</P>
-   *
-   * @param {Class} commandClassRef a reference to the <code>Class</code> of the <code>ICommand</code>.
-   */
-  addSubCommand: function(commandClassRef/*Class*/) {
-    this.subCommands.push(commandClassRef);
-  },
-
-  /**
-   * Registers the callback for a parent <code>AsyncMacroCommand</code>.
-   *
-   * @param {Function} value The <code>AsyncMacroCommand</code> method to call on completion
-   */
-  setOnComplete: function (value /*Function*/) {
-    this.onComplete = value.createDelegate(this);
-  },
-
-  /**
-   * Starts execution of this <code>AsyncMacroCommand</code>'s <i>SubCommands</i>.
-   * <P>
-   * The <i>SubCommands</i> will be called in First In/First Out (FIFO) order.
-   * </P>
-   *
-   * @param {Puremvc.patterns.Notification} notification the <code>Notification</code> object to be passed to each <i>SubCommand</i>.
-   */
-  execute: function(notification /*Puremvc.patterns.Notification*/) {
-    this.notification = notification;
-    this.nextCommand();
-  },
-
-  /**
-   * Execute this <code>AsyncMacroCommand</code>'s next <i>SubCommand</i>.
-   *
-   * <P>
-   * If the next <i>SubCommand</i> is asynchronous, a callback is registered for
-   * the command completion, else the next command is run.</P>
-   */
-  nextCommand: function() {
-    if (this.subCommands.length > 0) {
-      var commandClassRef/*Class*/ = this.subCommands.shift();
-      var commandInstance/*Object*/ = new commandClassRef();
-      var isAsync/*Boolean*/ = commandInstance.isAsyncCommand;
-
-      if (isAsync) {
-        commandInstance.setOnComplete(this.nextCommand.createDelegate(this));
-      }
-      commandInstance.initializeNotifier(this.multitonKey);
-      commandInstance.execute(this.notification);
-
-      if (!isAsync) {
-        this.nextCommand();
-      }
-    }
-    else {
-      if (this.onComplete != null) {
-        this.onComplete();
-      }
-
-      this.notification = null;
-      this.onComplete = null;
-    }
+	
+ 	/* implementation end */
+ 	 
+	// export the AsyncCommand utility classes to the
+	// org.puremvc.js.multicore.patterns.command namespace
+	var _classes = {
+    'AsyncCommand': AsyncCommand,
+    'AsyncMacroCommand': AsyncMacroCommand
+  };
+  for (var _class in _classes) {
+	  scope['org']['puremvc']['js']['multicore']['patterns']['command'][_class] = _classes[_class];
   }
-});
+})(this); // the 'this' parameter will resolve to global scope in all environments
 
